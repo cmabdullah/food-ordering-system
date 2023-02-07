f578f1a5fb77b04608ea5f087b06bd4b46d7760c
Khan, C M Abdullah <cmabdullah21@gmail.com>
2023-02-08 00:24:33 +0600

    step 23 testing order domain service logic part 1

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
@@ -35,7 +35,7 @@ public class OrderCreateCommandHandler {
 		OrderCreatedEvent orderCreatedEvent = orderCreateHelper.persistOrder(createOrderCommand);
 		orderCreatedPaymentRequestMessagePublisher.publish(orderCreatedEvent);
 		log.info("Order is created with id: {}", orderCreatedEvent.getOrder().getId().getValue());
-		return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder());
+		return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder(), "Order created successfully");
 	}
 
 }
