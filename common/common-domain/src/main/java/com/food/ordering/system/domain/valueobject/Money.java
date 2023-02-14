3b475cffdba9a098168cfaf9eeffe96911f611b2
Khan, C M Abdullah <cmabdullah21@gmail.com>
2023-02-14 23:51:51 +0600

    step43 domain core module adding aggregate root entity and value objects, added payment domain entity and value objects

--- common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -14,6 +14,7 @@ public class Money {
 		this.amount = amount;
 	}
 
+	//typo -> isGreaterThanZero
 	public boolean isGreaterThenZero() {
 		return amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;
 	}
