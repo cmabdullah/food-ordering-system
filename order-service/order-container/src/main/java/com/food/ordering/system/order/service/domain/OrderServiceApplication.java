c3eb167aa7fb3e2d238f529beb6d0cb40b02af57
Khan, C M Abdullah <cmabdullah21@gmail.com>
2023-02-22 21:33:44 +0600

    step58 implementing application service implementing data access module

--- order-service/order-container/src/main/java/com/food/ordering/system/order/service/domain/OrderServiceApplication.java
+++ order-service/order-container/src/main/java/com/food/ordering/system/order/service/domain/OrderServiceApplication.java
@@ -7,11 +7,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = {
 		"com.food.ordering.system.order.service.dataaccess"
-//		, "com.food.ordering.system.dataaccess"
+		, "com.food.ordering.system.dataaccess"
 })
 @EntityScan(basePackages = {
 		"com.food.ordering.system.order.service.dataaccess"
-//		, "com.food.ordering.system.dataaccess"
+		, "com.food.ordering.system.dataaccess"
 })
 @SpringBootApplication(scanBasePackages = "com.food.ordering.system")
 public class OrderServiceApplication {
