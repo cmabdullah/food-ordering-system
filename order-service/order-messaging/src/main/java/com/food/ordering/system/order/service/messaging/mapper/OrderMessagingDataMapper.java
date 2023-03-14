12beaa760a633457399f4e19de17f9673a8332eb
Khan, C M Abdullah <cmabdullah21@gmail.com>
2023-03-14 19:24:08 +0600

    step78 refactoring order messaging module for outbox pattern part 1

--- order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/mapper/OrderMessagingDataMapper.java
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/mapper/OrderMessagingDataMapper.java
@@ -7,6 +7,7 @@ import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
 import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+import com.food.ordering.system.order.service.domain.outbox.model.payment.OrderPaymentEventPayload;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -92,6 +93,19 @@ public class OrderMessagingDataMapper {
 				.orderApprovalStatus(com.food.ordering.system.domain.valueobject.OrderApprovalStatus.valueOf(
 						restaurantApprovalResponseAvroModel.getOrderApprovalStatus().name()))
 				.failureMessages(restaurantApprovalResponseAvroModel.getFailureMessages())
+				.build();
+	}
+
+	public PaymentRequestAvroModel orderPaymentEventToPaymentRequestAvroModel(String sagaId, OrderPaymentEventPayload
+			orderPaymentEventPayload) {
+		return PaymentRequestAvroModel.newBuilder()
+				.setId(UUID.randomUUID().toString())
+				.setSagaId(sagaId)
+				.setCustomerId(orderPaymentEventPayload.getCustomerId())
+				.setOrderId(orderPaymentEventPayload.getOrderId())
+				.setPrice(orderPaymentEventPayload.getPrice())
+				.setCreatedAt(orderPaymentEventPayload.getCreatedAt().toInstant())
+				.setPaymentOrderStatus(PaymentOrderStatus.valueOf(orderPaymentEventPayload.getPaymentOrderStatus()))
 				.build();
 	}
 }
